aacf70491f3c75690c5782e0e629847ed8c86418
lukaskelley <lukaskelley412@gmail.com>
2022-11-25 03:58:57 +0900

    time interval

---
+++ src/Components/Light/index.tsx
@@ -0,0 +1,19 @@
+import "./Light.css";
+const Light = (props:LightProps) => {
+    return(
+        <div
+            className="light"
+            style={{backgroundColor: props.color, opacity: props.active ? 1 : 0.4}}
+        >
+        </div>
+    )
+    
+}
+
+
+export declare interface LightProps {
+    color : string;
+    active: boolean;
+}
+
+export default Light;
